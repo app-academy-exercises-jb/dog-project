fadfef454a4db3d1a03db696e7580ce2a58abb03
Jorge Barreto <jrbd93@gmail.com>
2019-11-08 13:43:41 -0500

    final

---
+++ .history/lib/dog_20191108130947.rb
@@ -0,0 +1,33 @@
+class Dog
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name, @breed, @age, @bark, @favorite_foods = name, breed, age, bark, favorite_foods
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age
+    end
+
+    def age=(num)
+        @age = num
+    end
+
+    def bark
+        @age > 3 ? @bark.upcase : @bark.downcase
+    end
+
+    def favorite_foods
+        @favorite_foods
+    end
+
+    def favorite_foods?(food)
+        @favorite_foods.any? { |fav| fav.casecmp(food) == 0 }
+    end
+end
